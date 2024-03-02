380f995b4c62b591c11b44a8555476219191f72c
Lingwuxin <57278566+Lingwuxin@users.noreply.github.com>
2024-03-02 11:00:14 +0800

    开始添加窗口最小化至系统任务栏托盘功能，发现bug，系统息屏后播放设备切换异常

--- ScreenSoundSwitch/MainForm.cs
+++ ScreenSoundSwitch/MainForm.cs
@@ -235,98 +235,7 @@ namespace ScreenSoundSwitch
             }
             return false;
         }
-        /*
-        bool GetAudioDeviceAllProcessId()
-        {
-            List<int> processIdList=new List<int>();
-            foreach (var deviceInfo in deviceInfoDict)
-            {
-                if (deviceInfo.Value.AudioSessionManager.Sessions == null)
-                {
-                    continue;
-                }
-                for (int i = deviceInfo.Value.AudioSessionManager.Sessions.Count; i > 0; i--)
-                {
-                    if (deviceInfo.Value.AudioSessionManager.Sessions[i - 1].State != AudioSessionState.AudioSessionStateActive)
-                    {
-                        continue;
-                    }
-                    if (deviceInfo.Value.AudioSessionManager.Sessions[i - 1].GetProcessID == 0)
-                    {
-                        continue;
-                    }
-                    //判断进程是否已经被添加到processInfoDict
-                    if (processInfoDict.ContainsKey((uint)deviceInfo.Value.AudioSessionManager.Sessions[i - 1].GetProcessID))
-                    {
-                        processInfoDict[(uint)deviceInfo.Value.AudioSessionManager.Sessions[i - 1].GetProcessID].IsUsing = true;
-                        continue;
-                    }
-                    else
-                    {
-                        Process process = Process.GetProcessById((int)deviceInfo.Value.AudioSessionManager.Sessions[i - 1].GetProcessID);
-                        ProcessInfo processInfo = new ProcessInfo();
-                        processInfo.IsUsing = true;
-                        processInfo.process = process;
-                        processIdList.Add(process.Id);
-                    }
-                }
-            }
-            foreach(var processId in processIdList)
-            {
-                if(!processInfoDict.ContainsKey((uint)processId))
-                {
-                    processInfoDict.Remove((uint)processId);
-                }
-            }
-            return true;
-        }
-        bool IsUsingAudioDeviceByProcessId(int processId)
-        {
-            foreach (var deviceInfo in deviceInfoDict)
-            {
-                if (deviceInfo.Value.AudioSessionManager.Sessions == null)
-                {
-                    continue;
-                }
-                for (int i = deviceInfo.Value.AudioSessionManager.Sessions.Count; i > 0; i--)
-                {
-                    if (deviceInfo.Value.AudioSessionManager.Sessions[i - 1].State != AudioSessionState.AudioSessionStateActive)
-                    {
-                        continue;
-                    }
-                    if (deviceInfo.Value.AudioSessionManager.Sessions[i - 1].GetProcessID == 0)
-                    {
-                        continue;
-                    }
-                    if (deviceInfo.Value.AudioSessionManager.Sessions[i - 1].GetProcessID == processId)
-                    {
-                        return true;
-                    }
-                }
-            }
-            return false;
-        }
 
-        static int GetAudioDeviceProcessId(MMDevice device)
-        {
-            if (device.AudioSessionManager.Sessions != null)
-            {
-                // 获取正在使用音频设备的进程 ID
-                for (int i = 0; i < device.AudioSessionManager.Sessions.Count; i++)
-                {
-                    var session = device.AudioSessionManager.Sessions[i];
-                    if (session.State == AudioSessionState.AudioSessionStateActive)
-                    {
-                        using (var process = Process.GetProcessById((int)session.GetProcessID))
-                        {
-                            return process.Id;
-                        }
-                    }
-                }
-            }
-            return -1; // 如果没有进程正在使用音频设备，则返回 -1
-        }
-      */
         private void Watcher_ForegroundProcessChanged(int processId)
         {
             /*
@@ -384,7 +293,7 @@ namespace ScreenSoundSwitch
         private void MainForm_Load(object sender, EventArgs e)
         {
             // 获取所有显示器和它们对应的播放设备
-            AudioDeviceEnumerator audioDeviceEnumerator = new AudioDeviceEnumerator();
+            AudioDeviceManger audioDeviceEnumerator = new AudioDeviceManger();
             deviceCollection = audioDeviceEnumerator.GetDevices();
             foreach (var device in deviceCollection)
             {
