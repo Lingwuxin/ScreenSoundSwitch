1da239202071b7a275a23d9600ac8ed03a9662c0
Lingwuxin <57278566+Lingwuxin@users.noreply.github.com>
2024-10-06 12:41:07 +0800

    实现VolumeControl.volumeTraceBar响应系统音量变化

---
+++ AudioDeviceInfoTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AudioDeviceInfoTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AudioDeviceInfoTest")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("37de40c2-1451-4de7-83d4-453645ccbda2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
